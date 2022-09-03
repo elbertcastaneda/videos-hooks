dc517a30d9ad3379676b74069c5476bf365682fb
Elbert Castañeda <elbert.castaneda@gmail.com>
2022-09-03 15:21:37 -0700

    Change label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form className="ui form" onSubmit={onSubmit} >
         <div className="field">
-          <label htmlFor="searchVideo">Search For a Video</label>
+          <label htmlFor="searchVideo">Video Search</label>
           <input
             id="searchVideo"
             type="text"
